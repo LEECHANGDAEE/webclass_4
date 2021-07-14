7dacada1f295786fddedf602034a9396710c8f56
LEECHANGDAEE <86697048+LEECHANGDAEE@users.noreply.github.com>
2021-07-14 16:57:23 +0900

    7/14 4ban class commit1

--- accountapp/views.py
+++ accountapp/views.py
@@ -19,16 +19,15 @@ def hello_world(request):
         new_model.text = temp
         new_model.save()
 
-
         return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
         data_list = NewModel.objects.all()
         return render(request, 'accountapp/hello_world.html',
                       context={'data_list': data_list})
 
+
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
-
