c25b750e9791472f155cc8655b4bd38fc9da81b5
LEECHANGDAEE <86697048+LEECHANGDAEE@users.noreply.github.com>
2021-07-20 11:53:45 +0900

    7/20 4ban class commit2

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name = 'accountapp'
+
+
+
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
@@ -16,5 +19,6 @@ urlpatterns = [
 
     path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
     path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', AccountDeleteView.as_view(), name='delete'),
 
 ]
