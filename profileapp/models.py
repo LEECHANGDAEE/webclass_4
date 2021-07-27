2d18186816131a75ca2bd6b709d202c5ed5c1d30
LEECHANGDAEE <86697048+LEECHANGDAEE@users.noreply.github.com>
2021-07-27 11:41:26 +0900

    7/27 4ban class commit2

--- profileapp/models.py
+++ profileapp/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
 
 class Profile(models.Model):
@@ -10,6 +11,3 @@ class Profile(models.Model):
     image = models.ImageField(upload_to='profile/', null=True)
     nickname = models.CharField(max_length=30, unique=True, null=True)
     message = models.CharField(max_length=200, null=True)
-
-
-
